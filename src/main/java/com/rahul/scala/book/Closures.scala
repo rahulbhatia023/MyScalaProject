4d0e13fdce69c1d753123d42666617fe7bf45867
rahulbhatia023 <rahul.bhatia023@gmail.com>
2017-03-15 14:13:43 +0530

    .

---
+++ src/main/java/com/rahul/scala/book/Closures.scala
@@ -0,0 +1,20 @@
+package com.rahul.scala.book
+
+object Closures extends App {
+  var more = 10
+
+  val functionLiteral1 = (x: Int) => x + more
+
+  println(functionLiteral1(20))
+
+  more = 20
+  println(functionLiteral1(20))
+
+  val list = List(1, 2, 3, 4, 5)
+
+  var sum = 0
+
+  list.foreach(sum += _)
+
+  println(sum)
+}
