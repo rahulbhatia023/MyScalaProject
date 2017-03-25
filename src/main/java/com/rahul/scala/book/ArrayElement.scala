43d940754ea399036d0dfb54b58efd4ef7e38c38
rahulbhatia023 <rahul.bhatia023@gmail.com>
2017-03-25 18:31:21 +0530

    Parametric Fields

--- src/main/java/com/rahul/scala/book/ArrayElement.scala
+++ src/main/java/com/rahul/scala/book/ArrayElement.scala
@@ -1,8 +1,6 @@
 package com.rahul.scala.book
 
-class ArrayElement(arrayContents: Array[Int]) extends Element {
-  override def contents(): Array[Int] = arrayContents
-}
+class ArrayElement(val contents: Array[Int]) extends Element
 
 object ArrayElement extends App {
   val arrayElement: Element = new ArrayElement(Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
