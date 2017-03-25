438f04c7dd43927bb89ad87c006f4f517ed45373
rahulbhatia023 <rahul.bhatia023@gmail.com>
2017-03-25 16:25:47 +0530

    Parameterless Methods

---
+++ src/main/java/com/rahul/scala/book/ParameterlessMethods.scala
@@ -0,0 +1,9 @@
+package com.rahul.scala.book
+
+object ParameterlessMethods extends App {
+  def contents: Array[Int] = Array(1, 1, 3, 4, 5, 6)
+
+  def size: Int = contents.length
+
+  println(size)
+}
