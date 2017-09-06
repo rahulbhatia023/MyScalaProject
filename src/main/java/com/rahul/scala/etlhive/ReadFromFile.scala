83771c6cddfbf44abf6429b93343dc50b2f70e32
Rahul Bhatia <rahul.bhatia023@gmail.com>
2017-09-07 00:22:44 +0530

    .

--- src/main/java/com/rahul/scala/etlhive/ReadFromFile.scala
+++ src/main/java/com/rahul/scala/etlhive/ReadFromFile.scala
@@ -6,9 +6,13 @@ object ReadFromFile extends App {
   val filePath = "/home/rahul/Softwares/hadoop-2.8.0/data/employee.txt"
   val lines = Source.fromFile(filePath).getLines()
 
-  var counter = 1
+  var counterLines = 1
   for (line <- lines) {
-    println("Line %d: %s".format(counter, line))
-    counter = counter + 1
+    println("Line %d: %s".format(counterLines, line))
+
+    val Array(employeeId, firstName, lastName, age, role, departmentID) = line.split(",")
+    println(s"$employeeId, $firstName, $lastName, $age, $role, $departmentID")
+
+    counterLines = counterLines + 1
   }
 }
